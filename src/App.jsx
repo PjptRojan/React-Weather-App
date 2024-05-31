ff2fa5468ace649fe38e2885d25b1d847e2eaaa5
RojanPrajapati-Outcode <rojan.prajapati@outcodesoftware.com>
2024-05-31 14:27:21 +0545

    remove console.log statements

--- src/App.jsx
+++ src/App.jsx
@@ -13,32 +13,28 @@ function App() {
 
   const handleOnSearchChange = (searchData) => {
     const [lat, long] = searchData.value.split(" ");
-    setIsLoading(true);
+
     const currentWeatherFetch = fetch(
       `${CURRENT_WEATHER_API_URL}/weather?lat=${lat}&lon=${long}&appid=${CURRENT_WEATHER_API_KEY}&units=metric`
     );
-    console.log("weatherfetch", currentWeatherFetch);
-    const status = currentWeatherFetch.status;
-    console.log("promise status is", status);
+
     const forecastFetch = fetch(
       `${CURRENT_WEATHER_API_URL}/forecast?lat=${lat}&lon=${long}&appid=${CURRENT_WEATHER_API_KEY}&units=metric`
     );
 
+    setIsLoading(true);
+
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
         const weatherResponse = await response[0].json();
-        console.log("1", weatherResponse);
         const forecastResponse = await response[1].json();
-        console.log(forecastResponse);
+
         setCurrentWeather({ city: searchData.label, ...weatherResponse });
         setForecast({ city: searchData.label, ...forecastResponse });
+        setIsLoading(false);
       })
       .catch((error) => console.log(error));
-    setIsLoading(false);
   };
-
-  console.log("currentWeather", currentWeather);
-  console.log("forecast", forecast);
 
   return (
     <div className="container bg-[#d5d4d4] py-10 mt-10 rounded-md min-h-[150px]">
