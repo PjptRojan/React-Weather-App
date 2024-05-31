bc3ab4eb167b47ad4d5037bc6de827525ad70e67
RojanPrajapati-Outcode <rojan.prajapati@outcodesoftware.com>
2024-05-31 15:23:53 +0545

    add env file for api keys and add env file in gitignore

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -10,5 +10,3 @@ export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
 export const CURRENT_WEATHER_API_URL =
   "https://api.openweathermap.org/data/2.5";
-
-export const CURRENT_WEATHER_API_KEY = "d9fc32960b886a5f5d98bfddab73f2d9";
